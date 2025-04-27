2ae944179c2c8a563089e9344fc38ef201a70620
Mayté Mejía <maytemejia@live.com.mx>
2025-04-26 20:29:51 -0600

    Add update and find passenger and add test for this

--- app/main.py
+++ app/main.py
@@ -19,6 +19,14 @@ async def list_passengers():
   query = passengers.select()
   return await database.fetch_all(query)
 
+@app.get("/passengers/{id}")
+async def get_passenger(id: int):
+  query = passengers.select().where(passengers.c.id == id)
+  passenger = await database.fetch_one(query)
+  if passenger is None:
+    raise HTTPException(status_code=404, detail="Passenger not found")
+  return passenger
+
 @app.post("/passengers")
 async def create_pasessenger(request: Request):
   data = await request.json()
@@ -28,7 +36,6 @@ async def create_pasessenger(request: Request):
 
   if not name or not last_name or not birthdate:
     raise HTTPException(status_code=400, detail="Faltan campos obligatorios")
-
   query = passengers.insert().values(name=name, last_name=last_name, birthdate=parse_date(birthdate))
 
   try:
@@ -36,6 +43,24 @@ async def create_pasessenger(request: Request):
     return {"id": new_id, "name": name, "last_name": last_name, "birthdate": birthdate}
   except Exception as e:
     raise HTTPException(status_code=400, detail="Passenger register was not created")
+
+@app.put("/passengers/{id}")
+async def update_passenger(id: int, request: Request):
+  data = await request.json()
+  name = data.get("name")
+  last_name = data.get("last_name")
+  birthdate = data.get("birthdate")
+
+  if not name or not last_name or not birthdate:
+    raise HTTPException(status_code=400, detail="Faltan campos obligatorios")
+  passenger = await get_passenger(id)
+
+  if passenger is None:
+    raise HTTPException(status_code=404, detail="Passenger not found")
+
+  query = passengers.update().where(passengers.c.id == id).values(name=name, last_name=last_name, birthdate=parse_date(birthdate))
+  await database.execute(query)
+  return {"id": id, "name": name, "last_name": last_name, "birthdate": birthdate}
 
 def parse_date(str):
   return datetime.strptime(str, '%Y-%m-%d').date()
--- test/main_test.py
+++ test/main_test.py
@@ -14,6 +14,27 @@ async def setup_and_teardown():
   await database.disconnect()
 
 @pytest.mark.asyncio
+async def test_list_passengers():
+  transport = ASGITransport(app)
+  async with AsyncClient(transport=transport, base_url="http://test") as ac:
+    response = await ac.get("/passengers")
+  assert response.status_code == 200
+
+@pytest.mark.asyncio
+async def test_get_passenger():
+  transport = ASGITransport(app)
+  async with AsyncClient(transport=transport, base_url="http://test") as ac:
+    response = await ac.get("/passengers/1")
+  assert response.status_code == 200
+
+@pytest.mark.asyncio
+async def test_get_passenger_filed():
+  transport = ASGITransport(app)
+  async with AsyncClient(transport=transport, base_url="http://test") as ac:
+    response = await ac.get("/passengers/1000000")
+  assert response.status_code == 404
+
+@pytest.mark.asyncio
 async def test_create_passenger():
   transport = ASGITransport(app)
   async with AsyncClient(transport=transport, base_url="http://test") as ac:
@@ -30,3 +51,42 @@ async def test_create_passenger():
   assert data["last_name"] == "Arango"
   assert data["birthdate"] == "2000-03-12"
   assert "id" in data
+
+@pytest.mark.asyncio
+async def test_create_passenger_filed():
+  transport = ASGITransport(app)
+  async with AsyncClient(transport=transport, base_url="http://test") as ac:
+    payload = {
+      "name": "Jeronimo",
+      "last_name": "Arango",
+    }
+    response = await ac.post("/passengers", json=payload)
+  assert response.status_code == 400
+
+@pytest.mark.asyncio
+async def test_update_passenger():
+  transport = ASGITransport(app)
+  async with AsyncClient(transport=transport, base_url="http://test") as ac:
+    payload = {
+      "name": "Jeronimo",
+      "last_name": "Montes",
+      "birthdate": "2000-04-12"
+    }
+    response = await ac.put("/passengers/1", json=payload)
+
+  assert response.status_code == 200
+  data = response.json()
+  assert data["name"] == "Jeronimo"
+  assert data["last_name"] == "Montes"
+  assert data["birthdate"] == "2000-04-12"
+
+@pytest.mark.asyncio
+async def test_update_passenger_filed():
+  transport = ASGITransport(app)
+  async with AsyncClient(transport=transport, base_url="http://test") as ac:
+    payload = {
+      "name": "Jeronimo",
+      "last_name": "Arango",
+    }
+    response = await ac.put("/passengers/1000000", json=payload)
+  assert response.status_code == 400
